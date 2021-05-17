801fd860748fb4147c41dc394f804221811a5b33
JosiasAurel <ndjosiasaurel@gmail.com>
2021-05-17 20:14:27 +0100

    update js and output elements

--- static/index.js
+++ static/index.js
@@ -5,19 +5,25 @@ function createAndAppendUrl(url, shortUrl) {
 
     // create elementss
     let span = document.createElement("span");
-    let p1 = document.createElement("p");
-    let p2 = document.createElement("p");
+    let p1 = document.createElement("a");
+    let p2 = document.createElement("a");
 
-    // set element values
+    // set element text values
     p1.innerText = shortUrl;
     p2.innerText = url;
 
-    // add p as child of span
+    // set element href values
+    p1.href = shortUrl;
+    p2.href = url;
+
+    // add a as child of span
     span.appendChild(p1);
     span.appendChild(p2);
 
-    // get parent URLs container
+    // add style class to span
+    span.classList.add("short");
 
+    // get parent URLs container
     const urlsContainer = document.getElementById("shorts");
 
     urlsContainer.appendChild(span);
@@ -26,7 +32,8 @@ function createAndAppendUrl(url, shortUrl) {
 async function shortenUrl(url) {
     let res = await fetch(`shorten?url=${url}`, {method: "POST"});
     let data = await res.json();
-    console.log(data);
+
+    createAndAppendUrl(url, `${location.href}${data.url}`);
 }
 
 formEl.addEventListener("submit", (event) => {
