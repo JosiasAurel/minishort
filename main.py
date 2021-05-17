291fec6f1ce7ca55c29e8a399b7961c647c5f360
JosiasAurel <ndjosiasaurel@gmail.com>
2021-05-17 20:24:43 +0100

    crud

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.encoders import jsonable_encoder
 
-from pydantic import BaseModel
+from deta import Deta
+
 import secrets
 
-app = FastAPI()
+app = FastAPI()  # create app instance
+
+deta = Deta("")  # provide project key
+
+db = deta.Base("minishort")  # create a DetaBase
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 pages = Jinja2Templates(directory="pages")
-
-
-class URLModel(BaseModel):
-    url: dict
 
 
 @app.get("/")
@@ -27,4 +27,11 @@ async def get_home(request: Request):
 @app.post("/shorten")
 async def shorten_url(url):
     # data = jsonable_encoder(url)
+    db.put(url, key=url)
     return {"url": secrets.token_urlsafe(6)}
+
+
+@app.get("/{url}")
+async def redirect_to_(url):
+    red_ = db.get(url)
+    return RedirectResponse(red_)
